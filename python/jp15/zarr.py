0e8eccb3813428cce4c4e5348ed89c463d87f15a
Almar Klein <almar@almarklein.org>
2026-07-26 12:03:56 +0200

    tweak compat module

--- python/jp15/zarr.py
+++ python/jp15/zarr.py
@@ -1,2 +1,7 @@
-from codecs.zarr import *  # noqa: F403
+import warnings
+from .codecs.zarr import *  # noqa: F403
+
 # TODO: backwards compat (juli 2026); remove this file after a while
+warnings.warn(
+    "jp15.zarr has moved to jp15.codecs.zarr - jp15.zarr still works but will be removed in a future version"
+)
